36dfebab25507e80f4ce789a8481fc184768b43c
Shashank Shekhar <sunnyrocks1122@gmail.com>
2024-10-21 20:22:08 +0530

    Update for URL

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
       'Content-Type': 'application/json',
     });
   }
-    // private baseUrl = 'api';
+  // private baseUrl = 'api';
   private baseUrl = environment.baseUrl;
 
 
